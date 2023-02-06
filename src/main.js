4f9f1e0a617b9e88ce742838079db2135ec9da66
Israel Oriente <israelnunesoriente@gmail.com>
2023-02-06 15:22:52 +0000

    feat(pinia): init pinia store

--- src/main.js
+++ src/main.js
@@ -2,12 +2,19 @@ import Vue from "vue";
 import App from "./App.vue";
 import vuetify from "./plugins/vuetify";
 import router from "./router";
+import VueCompositionAPI from "@vue/composition-api";
+import { createPinia, PiniaVuePlugin } from "pinia";
 import "./theme/main.scss";
+
+const pinia = createPinia();
+Vue.use(PiniaVuePlugin);
+Vue.use(VueCompositionAPI);
 
 Vue.config.productionTip = false;
 
 new Vue({
   vuetify,
   router,
+  pinia,
   render: (h) => h(App),
 }).$mount("#app");
