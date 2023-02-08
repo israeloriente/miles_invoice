3ca68df5cd2a9c1166ef8ffd912a8f8a713b46f6
Israel Oriente <israelnunesoriente@gmail.com>
2023-02-08 11:17:27 +0000

    fix: removing old repo things 💩

--- src/main.js
+++ src/main.js
@@ -2,17 +2,13 @@ import Vue from "vue";
 import App from "./App.vue";
 import vuetify from "./plugins/vuetify";
 import router from "./router";
-import { createPinia, PiniaVuePlugin } from "pinia";
 import "./theme/main.scss";
 import "./theme/animations.scss";
 
-const pinia = createPinia();
-Vue.use(PiniaVuePlugin);
 Vue.config.productionTip = false;
 
 new Vue({
   vuetify,
   router,
-  pinia,
   render: (h) => h(App),
 }).$mount("#app");
--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,6 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import HomeView from "../views/HomeView.vue";
-import LoginPage from "../views/auth/LoginPage.vue";
-import auth_guard from "@/router/guards/auth";
 
 Vue.use(VueRouter);
 
@@ -16,26 +14,11 @@ const routes = [
     name: "home",
     component: HomeView,
   },
-  {
-    path: "/login",
-    name: "login",
-    component: LoginPage,
-  },
-  {
-    path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
-  },
 ];
 const router = new VueRouter({
   mode: "history",
   base: process.env.BASE_URL,
   routes,
 });
-router.beforeEach((to, from, next) => auth_guard(to, from, next));
 
 export default router;
